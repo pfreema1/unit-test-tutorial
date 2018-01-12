e1cb547309886ea594b26c5ebe060c0380278ceb
Paul <pfreema1@gmail.com>
2018-01-12 16:07:47 -0500

    moo

--- app/assets/scripts/order-total.test.js
+++ app/assets/scripts/order-total.test.js
@@ -10,38 +10,32 @@ it("Quantity", () => {
   })).toBe(6);
 });
 
-if(orderTotal({
-  items: [
-    { "name": "Dragon Candy", price: 2, quantity: 3 }
-  ]
-}) !== 6) {
-  throw new Error("Check fail: Quantity");
-}
+it("No quantity specified", () => {
+  expect(orderTotal({
+    items: [
+      { "name": "dragon poo", price: 3 }
+    ]
+  })).toBe(3);
+});
 
-if(orderTotal({
-  items: [
-    { "name": "dragon poo", price: 3 }
-  ]
-}) !== 3) {
-  throw new Error("Check fail:  No quantity specified");
-}
+it("Happy path (example 1)", () => {
+  expect(orderTotal({
+    items: [
+      { name: "Dragon food", price: 8, quantity: 1 },
+      { name: "Dragon cage (small)", price: 800, quantity: 1 }
+    ]
+  })).toBe(808);
+});
 
-if(orderTotal({
-  items: [
-    { name: "Dragon food", price: 8, quantity: 1 },
-    { name: "Dragon cage (small)", price: 800, quantity: 1 }
-  ]
-}) !== 808) {
-  throw new Error("Check fail: Happy path (example 1)");
-}
+it("Happy path (example 2)", () => {
+  expect(orderTotal({
+    items: [
+      { name: "Dragon collar", price: 20, quantity: 1 },
+      { name: "Dragon chew toy", price: 40, quantity: 1 }
+    ]
+  })).toBe(60);
+});
 
-if(orderTotal({
-  items: [
-    { name: "Dragon collar", price: 20, quantity: 1 },
-    { name: "Dragon chew toy", price: 40, quantity: 1 }
-  ]
-}) !== 60) {
-  throw new Error("Check fail: Happy path (example 2)");
-}
+
 
 
---
+++ app/assets/scripts/sandbox.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+const myKey = require("./helper.js");
+
+const result = 
+  fetch("https://vatapi.com/v1/country-code-check?code=DE", {
+    headers: {
+      "apikey": myKey
+    }
+    
+  })
+  .then(response => {
+    console.log(response);
+    return response.json(); 
+  })
+  .then(data => {
+    console.log(data.rates.standard);
+    return data.rates.standard.value
+  });
+
+// console.log(myKey);
